78e461d30f99d108245f617a74042313af29ae85
vanane <blackfox248@gmail.com>
2020-10-01 13:37:58 +0200

    Ajout des fichiers data

---
+++ config.py
@@ -0,0 +1,11 @@
+##############################
+# Variables de configuration #
+##############################
+pathToCaracteristiques = "./csv/caracteristiques-2018.csv"
+pathToLieux = "./csv/lieux-2018.csv"
+pathToUsagers = "./csv/usagers-2018.csv"
+pathToVehicules = "./csv/vehicules-2018.csv"
+
+filtresVehicules = {"01", "99"} # 01 est le code pour bicyclettes. 99 est le code pour piétons non-motorisés.
+filtresDepartements = {"440"} # Département 44
+##############################
---
+++ main.py
@@ -0,0 +1,50 @@
+import csv
+import io
+import geojson
+from config import *
+
+def importCSV(name):    
+    reader = open(name, mode="r", encoding="utf-8")
+    return reader
+
+
+fichierCaracteristiques = importCSV(pathToCaracteristiques)
+fichierLieux = importCSV(pathToLieux)
+fichierUsagers = importCSV(pathToUsagers)
+fichierVehicules = importCSV(pathToVehicules)
+
+accidentsVelos = dict()
+accidentsVelosNantes = dict()
+
+# Récupérer les numéros des accidents ayant comme victime cyclistes ou piétons équipés.
+# L'utilisation d'un dictionnaire permet de ne pas avoir de doublons.
+for row in csv.DictReader(fichierVehicules, delimiter=",", quotechar="\""):
+    if row["catv"] in filtresVehicules:
+        accidentsVelos.update( { row["Num_Acc"] : True } )
+
+# Récupérer les informations des accidents éligibles.
+for row in csv.DictReader(fichierCaracteristiques, delimiter=",", quotechar="\""):
+    if (row["Num_Acc"] in accidentsVelos) and (row["dep"] in filtresDepartements):
+        accidentsVelosNantes.update( { row["Num_Acc"] : row } )
+
+
+# Génération de la liste pour convertir en GeoJSON
+nbAccidents = len(accidentsVelosNantes)
+geoJsonFeatures = [] # Tableau contenant les Features créées, une Feature étant un objet de la carte (point, trait, polygone)
+
+for row in accidentsVelosNantes.values():
+    try:
+        pointAccident = geojson.Point((int(row["lat"]) / 100000, int(row["long"]) / 100000))
+        geoJsonFeatures.append(pointAccident)
+    except ValueError:
+        print("Error : ")
+        print(row)
+
+listePoints = geojson.FeatureCollection(geoJsonFeatures)
+
+# print("Il y a eu "+str(len(accidentsVelos))+" accidents de la route concernant des cyclistes en France. "+str(len(accidentsVelosNantes))+" en Loire-Atlantique.")
+print(listePoints)
+
+outputJson = open("output.json", "a")
+outputJson.write(str(listePoints))
+outputJson.close()
